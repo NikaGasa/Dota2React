555335c690fae135c4632e79c6add02da5aefe35
NikaGasa <93001446+NikaGasa@users.noreply.github.com>
2025-07-03 19:35:59 +0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import { fileURLToPath, URL } from "url";
 
 export default defineConfig({
-  base: "./", // 👈 ADD THIS LINE
+  base: "./", 
   plugins: [react()],
   resolve: {
     alias: {
